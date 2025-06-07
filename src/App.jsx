0729807ee587d8987c20cadf4e7e3d8af88cd4ed
akshay-ken <akshaybest3120@gmail.com>
2025-06-07 15:31:46 +0530

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-overpass text-9xl">Testing</h1>
+      <h1 className="text-9xl">regular</h1>
+    </>
+  );
+}
+
+export default App;
